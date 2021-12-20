6ae8a73b733bb136233f30eea10ffb98d9acb2c4
ZX Tan <tanzx_05@u.nus.edu>
2021-12-20 16:49:39 +0800

    download feature works!!!!

--- src/main/java/sg/edu/nus/LAPS/services/PDFGenerateService.java
+++ src/main/java/sg/edu/nus/LAPS/services/PDFGenerateService.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -24,23 +25,27 @@ public class PDFGenerateService {
         Document doc = new Document(PageSize.A4);
         PdfWriter.getInstance(doc, response.getOutputStream());
         doc.open();
-        PdfPTable table = new PdfPTable(list.size());
+        PdfPTable table = new PdfPTable(8);
         addTableHeader(table);
         for(LeaveApplication row:list){
             table.addCell(row.getLeaveId().toString());
-            table.addCell(row.getApprovalStatus().toString());
-            table.addCell(row.getContactDetails());
-            table.addCell(row.getCoveringEmp());
+            table.addCell(row.getLeaveType().getLeaveName());
             table.addCell(row.getFromDate().toString());
             table.addCell(row.getToDate().toString());
+            table.addCell(row.getContactDetails());
+            table.addCell(row.getCoveringEmp());
+            table.addCell(row.getApprovalStatus().toString());
+            table.addCell(row.getManagerComment());
+            
         }
         doc.add(table);
         doc.close();
     }
     private void addTableHeader(PdfPTable table) {
-        Stream.of("column header 1", "column header 2", "column header 3", "column header 4", "column header 5", "column header 6")
+        Stream.of("Leave Id", "Leave Type",  "From Date", "To Date", "Contact Details", "Covering Employee", "Approval Status", "Manager's Comment")
           .forEach(columnTitle -> {
             PdfPCell header = new PdfPCell();
+            header.setPhrase(new Phrase(columnTitle));
             table.addCell(header);
         });
     }
