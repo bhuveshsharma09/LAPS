0890e4fa0d0e0075e104c07a92e2608cda2c05a8
Steven Wong <ycstevenwong@gmail.com>
2021-12-23 15:42:28 +0800

    change v1

--- src/main/java/sg/edu/nus/LAPS/controller/StaffController.java
+++ src/main/java/sg/edu/nus/LAPS/controller/StaffController.java
@@ -92,8 +92,8 @@ public class StaffController {
         leaveApplicationService.saveLeaveApplication(LA);
 		List<LeaveApplication> last = leaveApplicationService.findAllLeaveApplicationSorted();
 		System.out.println(last.get(0).getLeaveId());
-		emailController.sendTheEmail(2, last.get(0).getLeaveId(), ApprovalStatus.APPLIED);
-        return "forward:/employee/leaveList/"+employee.getEmployeeId();
+		// emailController.sendTheEmail(2, last.get(0).getLeaveId(), ApprovalStatus.APPLIED);
+        return "forward:/employee/manageLeave/"+employee.getEmployeeId();
     }
     
     @RequestMapping("/manageLeave/{id}")
