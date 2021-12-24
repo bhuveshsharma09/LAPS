e995021f8e4b0aa24073db81a3f5acb302855c18
Bhuvesh Kumar <62707309+bhuveshsharma09@users.noreply.github.com>
2021-12-24 10:16:02 +0800

    added rest api which can help get information of all the employees who are on leave in the organisation

--- src/main/java/sg/edu/nus/LAPS/controller/EmployeeOnLeaveRestController.java
+++ src/main/java/sg/edu/nus/LAPS/controller/EmployeeOnLeaveRestController.java
@@ -12,7 +12,10 @@ import sg.edu.nus.LAPS.services.EmployeeService;
 import sg.edu.nus.LAPS.services.LeaveApplicationService;
 import sg.edu.nus.LAPS.validators.ClaimValidator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/all")
@@ -24,13 +27,39 @@ public class EmployeeOnLeaveRestController {
     LeaveApplicationService leaveApplicationService;
 
     @RequestMapping(value = "/onLeave")
-    public List<LeaveApplication> onLeave(Model model)
+    public List<Map> onLeave(Model model)
     {
-       // List<LeaveApplication> leaveApplicationList = leaveApplicationService.findAllById(0, ApprovalStatus.APPROVED);
+        List<LeaveApplication> leaveApplicationList = leaveApplicationService.findAllById(0);
+        List<LeaveApplication> approvedLeaves = new ArrayList<LeaveApplication>();
+        for (LeaveApplication l:leaveApplicationList
+        ) {
+            if (l.getApprovalStatus()==ApprovalStatus.APPROVED)
+            {
+                approvedLeaves.add(l);
+            }
 
-        //System.out.println(leaveApplicationList.getRemarks());
-       // model.addAttribute("leavelist",leaveApplicationList);
-         return null;
+
+        }
+        List<Map> leaveElements = new ArrayList<>();
+
+
+        for (LeaveApplication approvedLeave:approvedLeaves
+             ) {
+            Map<String, String> temp = new HashMap<>();
+            temp.put("Leave Id",String.valueOf(approvedLeave.getLeaveId()));
+            temp.put("From Date",String.valueOf(approvedLeave.getFromDate()));
+            temp.put("To Date",String.valueOf(approvedLeave.getToDate()));
+            temp.put("Employee Name",String.valueOf(approvedLeave.getEmployee().getName()));
+            temp.put("Contact Details",String.valueOf(approvedLeave.getContactDetails()));
+            temp.put("Covering Employee",String.valueOf(approvedLeave.getCoveringEmp()));
+            temp.put("Approval Status",String.valueOf(approvedLeave.getApprovalStatus()));
+
+            leaveElements.add(temp);
+
+
+        }
+
+        return leaveElements;
     }
 
 
